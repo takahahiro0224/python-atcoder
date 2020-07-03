158cf33cec96f1dcc2373079fa9df9c2dd1c8cb1
takahahiro0224 <hikusema189@keio.jp>
2020-07-03 19:28:01 +0900

    abc144 Cまで

--- sandbox/sample.py
+++ sandbox/sample.py
@@ -21,3 +21,24 @@ def factorization(n):
     return arr
 
 print(factorization(50))
+
+
+# 因数をlistで返す
+def factorizationList(n):
+    arr = []
+    tmp = n
+    for i in range(2, int(-(-n**0.5//1)) + 1):
+        if tmp % i == 0:
+            while tmp % i == 0:
+                tmp //= i
+                arr.append(i)
+
+    if tmp != 1:
+        arr.append(tmp)
+
+    if arr == []:
+        arr.append(n)
+
+    return arr
+
+print(factorizationList(50))
--- src/abc144/abc144_c.py
+++ src/abc144/abc144_c.py
@@ -1,2 +1,17 @@
 # https://atcoder.jp/contests/abc144/tasks/abc144_c
+
 n = int(input())
+
+# nを割れる整数を1からルートnまで列挙
+def divisors(num):
+   arr = []
+   for i in range(1, int(num**0.5) + 1):
+       if num % i == 0:
+           arr.append(i)
+           if i != num // i:
+               arr.append(num//i)
+   return arr
+
+n_div = divisors(n)
+arr_ = [int(i + n / i) for i in n_div]
+print(min(arr_) - 2)
