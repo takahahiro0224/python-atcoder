43dae25a7a63e4e24ec4b8448b7ea4cd1607551d
takahahiro0224 <hikusema189@keio.jp>
2020-07-14 23:33:39 +0900

    いったんコミット

--- src/abc147/abc147_c.py
+++ src/abc147/abc147_c.py
@@ -1,16 +1,10 @@
 # https://atcoder.jp/contests/abc147/tasks/abc147_c
 from typing import List, Tuple
 
+# 参考回答
+# https://atcoder.jp/contests/abc147/submissions/14837598
+# そんなにわかってない
 
-'''
-証言が以下の時
-[[(2, 1), (3, 0)], [(3, 1), (1, 0)], [(1, 1), (2, 0)]]
-1: 2は正直、3は不親切  
-2: 1は不親切、3は正直   
-3: 1は正直、2は不親切  この場合正直者は0人
-Xiの人が正直者と仮定して、 他の証言と照らし合わせる
-
-'''
 
 
 def io_readline() -> List[List[Tuple[int, ...]]]:
@@ -30,13 +24,19 @@ infos = io_readline()
 ans = 0
 for i in range(2 ** len(infos)):
     cnt = bin(i).count("1")
-    if cnt <= ans:
-        continue
-
-    for j in range(2 ** len(infos)):
+    if cnt <= ans: continue
+    for j in range(len(infos)):
+        # 1を左シフト
         if i & (1 << j):
+            for x, y in infos[j]:
+                if y == 1 and i % (1 << x-1): continue
+                if y == 0 and i % (1 << x-1) == 0: continue
+                break
+            else:
+                continue
+            break
+    else:
+        ans = cnt
 
-# 続きは後で
-
-
+print(ans)
 
