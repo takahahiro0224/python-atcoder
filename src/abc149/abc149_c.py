9b51f90e51b359b414c82063463152efccafd3d0
takahahiro0224 <hikusema189@keio.jp>
2020-07-30 14:54:43 +0900

    abc149

--- src/abc149/abc149_c.py
+++ src/abc149/abc149_c.py
@@ -1,2 +1,28 @@
 # https://atcoder.jp/contests/abc149/tasks/abc149_c
+
+
+def is_prime(num: int) -> bool:
+    # 6k +- 1 <= √n
+    if num <= 1:
+        return False
+
+    if num <= 3:
+        return True
+
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i+2) == 0:
+            return False
+        i += 6
+
+    return True
+
+
 x = int(input())
+for i in range(x,1000000):
+    if is_prime(i):
+        print(i)
+        break
